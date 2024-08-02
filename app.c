07fbfd7944377d1646c3264c2c934c4792c6c925
Aakarsh Dhar <dharaakarsh@gmail.com>
2024-08-02 19:57:33 -0400

    Base Commit
    Here's the basic idea for the code. Can print out a board with all the pieces on it.
    
    Data will be stored in an int array by mapping int values which represent pieces to positions on the board.

---
+++ app.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+void print_board(int *bp_ptr)
+{
+    printf("\n ------");
+
+    int i;
+    for (i = 0; i < 25; ++i)
+    {
+        if (i % 5 == 0)
+        {
+            printf("\n");
+        }
+        printf("%d", bp_ptr[i]);
+    }
+
+    printf("\n ------");
+    printf("\n Pawn = 1, Rook = 2, Knight = 3, Bishop = 4, Queen = 5, King = 6");
+}
+
+int take_input()
+{
+    return 0;
+}
+
+int *set_pieces(int piece, int position, int *bp_ptr, char is_setup)
+{
+    if (is_setup == 'y')
+    {
+        int i;
+        for (i = 0; i < 25; ++i)
+        {
+            bp_ptr[i] = 0;
+        }
+    }
+
+    else if (is_setup == 'n')
+    {
+        bp_ptr[position] = piece;
+    }
+    return bp_ptr;
+}
+
+int main(void)
+{
+    // 0 is top left, 4 is top right, 5 is below top left, 9 is below top right...
+    // 20 is the bottom left, 24 is the bottom right
+    // Pawn = 1, Rook = 2, Knight = 3, Bishop = 4, Queen = 5, King = 6
+    int board_pieces[25];
+    int *bp_ptr = board_pieces;
+    set_pieces(0, 0, bp_ptr, 'y');
+
+    int i;
+
+    // Setup top & bottom pawns
+    for (i = 5; i < 10; ++i)
+    {
+        set_pieces(1, i, bp_ptr, 'n');
+        set_pieces(1, (i + 10), bp_ptr, 'n');
+    }
+
+    // Set top side
+    set_pieces(2, 0, bp_ptr, 'n');
+    set_pieces(3, 1, bp_ptr, 'n');
+    set_pieces(4, 2, bp_ptr, 'n');
+    set_pieces(5, 3, bp_ptr, 'n');
+    set_pieces(6, 4, bp_ptr, 'n');
+
+    // Set top side
+    set_pieces(2, 20, bp_ptr, 'n');
+    set_pieces(3, 21, bp_ptr, 'n');
+    set_pieces(4, 22, bp_ptr, 'n');
+    set_pieces(5, 23, bp_ptr, 'n');
+    set_pieces(6, 24, bp_ptr, 'n');
+
+    print_board(bp_ptr);
+
+    return 0;
+}
