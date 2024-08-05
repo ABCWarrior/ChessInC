7b2ca05d7d100120fa0cd60824b5199f2ccc8f0b
Aakarsh Dhar <dharaakarsh@gmail.com>
2024-08-05 19:50:17 -0400

    Just added comments
    title :3

--- app.c
+++ app.c
@@ -2,11 +2,11 @@
 
 void take_input();
 
-
 void print_board(int *bp_ptr)
 {
     printf("\n------");
 
+    // Loops from 0-24 and prints the int value of the bp_ptr array there
     int i;
     for (i = 0; i < 25; ++i)
     {
@@ -17,13 +17,16 @@ void print_board(int *bp_ptr)
         printf("%d", bp_ptr[i]);
     }
 
+    // Information about int -> piece here
     printf("\n------");
     printf("\nPawn = 1, Rook = 2, Knight = 3, Bishop = 4, Queen = 5, King = 6");
+
+    // After printing, asks for input
     take_input();
 }
 
 void take_input()
-{    
+{
     int row, column;
     printf("\n------");
 
@@ -40,11 +43,14 @@ void take_input()
     // Return the position of the piece and whether it is valid to move
 
     printf("%d %d", row, column);
-
 }
 
 int *set_pieces(int piece, int position, int *bp_ptr, char is_setup)
 {
+    // First check if being called for setup
+    // If yes, then set the entire array to 0
+    // TODO: Might be able to remove the 'y' branch for optimization, but unsure right now
+
     if (is_setup == 'y')
     {
         int i;
@@ -54,17 +60,23 @@ int *set_pieces(int piece, int position, int *bp_ptr, char is_setup)
         }
     }
 
+    // If it's not setup, just set exactly the piece to the position specified
+
     else if (is_setup == 'n')
     {
         bp_ptr[position] = piece;
     }
+
+    // Return the bp_ptr array with all the pieces
+
     return bp_ptr;
 }
 
 int main(void)
 {
+    // Switched smtn to remove the buffer from stdout 
     setvbuf(stdout, NULL, _IONBF, 0);
-    printf("running");
+
     // 0 is top left, 4 is top right, 5 is below top left, 9 is below top right...
     // 20 is the bottom left, 24 is the bottom right
     // Pawn = 1, Rook = 2, Knight = 3, Bishop = 4, Queen = 5, King = 6
